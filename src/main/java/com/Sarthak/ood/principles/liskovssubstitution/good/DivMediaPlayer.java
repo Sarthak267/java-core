21bfd86881f2eaa3266dd71d3f75e49884c5fff8
Sarthak267 <94239913+Sarthak267@users.noreply.github.com>
2024-10-05 16:02:33 +0530

    Update DivMediaPlayer.java

--- src/main/java/com/Sarthak/ood/principles/liskovssubstitution/good/DivMediaPlayer.java
+++ src/main/java/com/Sarthak/ood/principles/liskovssubstitution/good/DivMediaPlayer.java
@@ -1,4 +1,4 @@
-package com.ramesh.ood.principles.liskovssubstitution.good;
+package com.sarthak.ood.principles.liskovssubstitution.good;
 
 /**
  * Div Media player extending behavior of super class Video Media Player. Perfect, LSP is not violated here.
